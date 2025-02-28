1b3f69dccaaa89a845641105a2eb540cb2325052
Dani Craig <dcraig1620@gmail.com>
2025-02-28 15:14:27 -0600

    finished mobile menu on home page

---
+++ js/nav.js
@@ -0,0 +1,28 @@
+const menuBtn = document.querySelector('#menubtn');
+const menuOpen = document.querySelector('#menubtn_open');
+const menuClose = document.querySelector('#menubtn_close');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+function toggleMenu() {
+    const isExpanded = menuBtn.getAttribute('aria-expanded') === 'true';
+
+    menuBtn.setAttribute('aria-expanded', !isExpanded);
+
+    mobileMenu.setAttribute('aria-hidden', isExpanded);
+    // console.log(isExpanded);
+    // console.log(!isExpanded);
+
+    if (!isExpanded) {
+        menuClose.style.display = 'flex';
+        menuOpen.style.display = 'none';
+        mobileMenu.classList.add('open');
+    } else {
+        menuClose.style.display = 'none';
+        menuOpen.style.display = 'flex';
+        mobileMenu.classList.remove('open');
+    }
+}
+
+menuBtn.addEventListener('click', () => {
+    toggleMenu();
+});
