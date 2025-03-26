1698624d034a394a23d00ba298214050895e84cb
Dani Craig <dcraig1620@gmail.com>
2025-03-26 15:19:12 -0500

    Attempted and failed to make extra images enlargement more accessible

--- js/enlarge.js
+++ js/enlarge.js
@@ -1,25 +1,24 @@
 // Get the modal
-var modal = document.getElementById('myModal');
+let modal = document.getElementById('myModal');
                 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// Get the <button> element that closes the modal
+let closeButton = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
+// Get all images
+let images = document.getElementsByTagName('img');
+let modalImg = document.getElementById("img01");
+let i;
+
+// When the user clicks on <button> (x), close the modal
+closeButton.onclick = function() { 
   modal.style.display = "none";
 }
 
-// Get all images and insert the clicked image inside the modal
-// Get the content of the image description and insert it inside the modal image caption
-var images = document.getElementsByTagName('img');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-var i;
+// Insert the clicked image inside the modal
 for (i = 0; i < images.length; i++) {
   images[i].onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     modalImg.alt = this.alt;
-    captionText.innerHTML = this.nextElementSibling.innerHTML;
   }
 }
